5b3ab706925b14be813d76f05e92d37181ef235d
machadoluan <machado.luandealmeida@gmail.com>
2025-04-21 22:46:12 +0000

    resolvendo conflitos

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://132.255.58.65:1352', 'http://localhost:4200'],
+   app.enableCors({
+    origin: ['https://app.devmchd.space', 'http://localhost:4200'],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
+
   
   await app.listen(process.env.PORT ?? 3000);
 }
